c90ad9e97c94208ba103931d147d1d01e04ab46c
AleGlez8 <AlejandraGE55@gmail.com>
2024-11-25 03:26:05 -0600

    Changes in MyAccount

--- src/screens/TermsAndConditions.js
+++ src/screens/TermsAndConditions.js
@@ -10,24 +10,26 @@ function TermsAndConditions({ navigation }) {
       </View>
       <View style={styles.content}>
         <View style={styles.termsContainer}>
+          <Text style={styles.text}></Text>
           <Text style={styles.text}>
-            • El usuario se compromete a proporcionar información veraz.
+            • Obligación de proporcionar información veraz: El usuario se compromete a proporcionar información precisa y actualizada al registrarse.
           </Text>
           <Text style={styles.text}>
-            • La contraseña del usuario debe mantenerse confidencial.
+            • Confidencialidad de la contraseña: El usuario es responsable de mantener la confidencialidad de su contraseña y de cualquier actividad que se realice bajo su cuenta.
           </Text>
           <Text style={styles.text}>
-            • Restricciones de uso: El usuario no debe utilizar la plataforma para actividades ilegales.
+            • Restricciones de uso: Se establecen las limitaciones en el uso de la aplicación, como la prohibición de realizar actividades ilegales o que puedan dañar la plataforma o a otros usuarios.
           </Text>
           <Text style={styles.text}>
-            • Limitación de responsabilidad: La plataforma no se hace responsable de daños indirectos.
+            • Limitación de responsabilidad: Establece los límites de responsabilidad de la plataforma en caso de cualquier daño o pérdida.
           </Text>
           <Text style={styles.text}>
-            • Ley aplicable y jurisdicción: Estos términos se rigen por las leyes de México.
+            • Ley aplicable y jurisdicción: Indica la ley aplicable y el fuero competente para resolver cualquier controversia.
           </Text>
           <Text style={styles.text}>
-            • Modificación de los términos: La plataforma puede modificar estos términos en cualquier momento.
+            • Modificación de los términos: Reserva el derecho de modificar los términos y condiciones en cualquier momento.
           </Text>
+          <Text style={styles.text}></Text>
           <Text style={styles.text}></Text>
           <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
             <Text style={styles.buttonText}>Aceptar</Text>
@@ -64,29 +66,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    paddingTop: 10,
+    paddingTop: 1,
   },
   termsContainer: {
     width: '80%',
     backgroundColor: '#D2AC8F',
     padding: 15,
+    paddingTop: 1,
     borderRadius: 30,
     marginBottom: 20,
     position: 'relative',
   },
   text: {
-    fontSize: 16,
+    fontSize: 15,
     color: '#000000',
-    marginBottom: 10,
+    marginBottom: 3,
     textAlign: 'left',
   },
   button: {
     position: 'absolute',
-    bottom: 10,
-    right: 10,
+    bottom: 15,
+    right: 30,
+    marginBottom: 10,
   },
   buttonText: {
-    fontSize: 15,
+    fontSize: 16,
     color: '#DD6B17',
     fontWeight: 'bold',
   },
